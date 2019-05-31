f8af46a5837df801a5629b025f968b29e1e80c96
Rafe Verkerk <rverkerk@csod.com>
2019-05-31 15:12:08 +1200

    Updated compiled code with centered bar-label

--- dist/frappe-gantt.js
+++ dist/frappe-gantt.js
@@ -823,7 +823,7 @@ var Bar = function () {
                 label.setAttribute('x', bar.getX() + bar.getWidth() + 5);
             } else {
                 label.classList.remove('big');
-                label.setAttribute('x', bar.getX() + bar.getWidth() / 2);
+                label.setAttribute('x', bar.getX() + bar.getWidth() / 2 - label.getBBox().width / 2);
             }
         }
     }, {
